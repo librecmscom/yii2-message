aac2a6841d50a68cb07d373f41ca5e819f18f550
xuchao <85825770@qq.com>
2017-03-31 12:03:32 +0800

    init

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\message;
+
+use Yii;
+
+/**
+ * Class Module
+ * @package yuncms\message
+ */
+class Module extends \yii\gii\Module
+{
+
+}
