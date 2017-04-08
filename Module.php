7fbabc2df0c1144d5f39917ac95154f347ba03f9
xuchao <85825770@qq.com>
2017-04-08 18:00:16 +0800

    init

--- Module.php
+++ Module.php
@@ -14,5 +14,38 @@ use Yii;
  */
 class Module extends \yii\gii\Module
 {
+    public $controllerNamespace = 'yuncms\message\controllers';
 
+    /**
+     * @var string the default route of this module. Defaults to `default`.
+     * The route may consist of child module ID, controller ID, and/or action ID.
+     * For example, `help`, `post/create`, `admin/post/create`.
+     * If action ID is not given, it will take the default value as specified in
+     * [[Controller::defaultAction]].
+     */
+    public $defaultRoute = 'message';
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * 注册语言包
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['message*'])) {
+            Yii::$app->i18n->translations['message*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
 }
---
+++ widgets/views/send_message.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin([
+    'id' => 'message-form',
+    'action' => ['/message/message/send']
+]); ?>
+<?= $form->field($model, 'username')->label(false)->hiddenInput(); ?>
+<?= $form->field($model, 'message')->label(false)->textarea(['class' => 'form-control']) ?>
+
+<?= Html::submitButton(Yii::t('user', 'Send'), ['class' => 'btn btn-primary']) ?>
+
+<?php ActiveForm::end(); ?>
+
